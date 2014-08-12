c1494b35c6627fb83f89666bbdb9a1be4bef0edb
Hondallica <hibikisound@gmail.com>
2014-08-12 21:01:11 +0900

    ヘッドバンキングに対応

--- HeavyMetal.user.js
+++ HeavyMetal.user.js
@@ -7,3 +7,4 @@
 // ==/UserScript==
 
 document.body.innerHTML = document.body.innerHTML.replace(/(ヘビメタ|ヘビーメタル)/g,'ヘヴィメタル');
+document.body.innerHTML = document.body.innerHTML.replace(/ヘッドバンキング/g,'ヘッドバンギング');
